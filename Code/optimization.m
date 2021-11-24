986ca7e4202659ef80580f3a27092dc49501356e
Poramat Chianvichai <64681582+PankidT@users.noreply.github.com>
2021-11-24 17:42:10 +0700

    Add optimization file with plot contour graph

---
+++ Code/optimization.m
@@ -0,0 +1,60 @@
+clear;clc
+x = 2.296200e-01;
+y = 3.970800e-01;
+z = 8.277400e-01;
+
+[theta, phi, fidelity] = optimization(x, y, z)
+
+function [theta, phi, max_fidelity] = optimization(x, y, z)
+    
+    i_mat = [1 0; 0 1];
+    x_mat = [0 1; 1 0];
+    y_mat = [0 -1i; 1i 0];
+    z_mat = [1 0; 0 -1];
+    rho_cart = 0.5*(i_mat + x_mat*x + y_mat*y + z_mat*z);
+    
+    theta = atan(y/x);
+    phi = atan(sqrt(x^2 + y^2)/z);
+    theta_dum = linspace(0, 180, 181);
+    phi_dum = linspace(0, 360, 361);
+    
+    index_theta=0;
+    index_phi=0;
+    for iter_theta = 0:180 
+        index_theta = index_theta + 1;
+        temp_theta = iter_theta*(pi/180);
+        for iter_phi = 0:360 
+            index_phi = index_phi + 1;
+            temp_phi = iter_phi*(pi/180);
+            
+            dum1 = cos(temp_theta/2)^2;
+            dum2 = exp(-1i*temp_phi)*cos(temp_theta/2)*sin(temp_theta/2);
+            dum3 = exp(1i*temp_phi)*cos(temp_theta/2)*sin(temp_theta/2);
+            dum4 = sin(temp_theta/2)^2;
+            rho_sp = [dum1 dum2; dum3 dum4];
+            
+            fidelity = sqrtm(sqrtm(rho_cart)*rho_sp*sqrtm(rho_cart));
+            fidelity = fidelity(1,1)+fidelity(2,2);
+            
+            fidel(index_theta, index_phi) = fidelity;
+        end
+        index_phi=0;
+    end
+    
+    [X, Y] = meshgrid(phi_dum, theta_dum);
+    surf(X, Y, real(fidel))
+    xlabel('Phi');
+    ylabel('Theta');
+    zlabel('Fidelity');
+    
+    max_fidelity = max(max(real(fidel)));
+    for row=1:181
+        for col=1:361
+            if real(fidel(row,col)) == max_fidelity
+                theta = row;
+                phi = col;
+            end
+        end
+    end
+    
+end
